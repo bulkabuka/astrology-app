cbd17da15217bf5811310183e9ba26e1e596c04e
Marat <marat18032005@mail.ru>
2023-03-20 15:40:33 +0300

    cho eto?

---
+++ BackTo.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace AstrologyApp
+{
+    public static class BackTo
+    {
+        public static string SelectedDate;
+        public static string DateTime;
+    }
+}
